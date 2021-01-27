114113171058ddd4c0f33ff017235e72463e5411
nucleotechcsl <nucleotechcsl@gmail.com>
2021-01-27 09:34:10 -0300

    tabela criada no heroku

--- index.js
+++ index.js
@@ -1,20 +1,24 @@
-const express = require("express");
-const app = express();
-const importaData = require("./data.json");
-let port = process.env.PORT || 3000;
-
-
-
-
-app.get("/", (req, res) =>{
-    res.send("Hello World")
-});
-
-
-app.get("/players", (req, res) =>{
-    res.send(importaData);
+const express=require("express");
+const bodyparser=require("body-parser");
+const cors=require("cors");
+const db=require('./query');
+const path = require('path');
+const app=express();
+var distDir = __dirname + "/dist/";
+app.use(express.static(__dirname + '/public'));
+app.use(express.static(distDir));
+const port = process.env.PORT || 3333;
+app.use(cors());
+app.use(bodyparser.json());
+app.use(
+bodyparser.urlencoded({
+extended:true,
 })
-
-app.listen(port, () =>{
-    console.log(`Example app is listening on port http://localhost:${port}`);
+);
+//database endpoints will be here
+app.get('/',(request,response)=>{
+response.json({info:'Node.js,Express, and Postgres API'});
 });
+app.listen(port,()=>{
+console.log(`App running on port ${port}.`);
+})
---
+++ query.js
@@ -0,0 +1,8 @@
+const Pool=require('pg').Pool 
+const pool=new Pool({
+user:'kithlzvmgcufht',
+host:'ec2-3-208-168-0.compute-1.amazonaws.com',
+database:'de8qsl9klapvur',
+password:'a53b62a87101b9a0f5dfd47dcb46d6fe767e68f1e0758ad7a34ea68a13210ed7',
+port:'5432',
+});
